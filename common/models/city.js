644519e7bfd505e07b0f460e232dd8e3ea2e008a
Stephen Smith <sns1215@gmail.com>
2016-10-18 10:02:32 -0400

    locked down the api calls to only allow for get requests

--- common/models/city.js
+++ common/models/city.js
@@ -1,5 +1,20 @@
 'use strict';
 
 module.exports = function(City) {
+  City.disableRemoteMethod('create', true);                // Removes (POST) /cities
+  City.disableRemoteMethod('upsert', true);                // Removes (PUT) /cities
+  City.disableRemoteMethod('replaceById', true);                // Removes (PUT) /cities
+  City.disableRemoteMethod('replaceOrCreate', true);                // Removes (PUT) /cities
+  City.disableRemoteMethod('upsertWithWhere', true);                // Removes (PUT) /cities
+  City.disableRemoteMethod('deleteById', true);            // Removes (DELETE) /cities/:id
+  City.disableRemoteMethod("updateAll", true);               // Removes (POST) /cities/update
+  City.disableRemoteMethod("updateAttributes", false);       // Removes (PUT) /cities/:id
+  City.disableRemoteMethod('createChangeStream', true);    // removes (GET|POST) /cities/change-stream
 
+  City.disableRemoteMethod('__create__sightings', false)
+  City.disableRemoteMethod('__upsert__sightings', false)
+  City.disableRemoteMethod('__delete__sightings', false)
+  City.disableRemoteMethod('__updateById__sightings', false)
+  City.disableRemoteMethod('__destroyById__sightings', false)
+  // City.disableRemoteMethod('__create__sightings', true)
 };
